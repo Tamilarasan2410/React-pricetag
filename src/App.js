f97f65b999ee435ae7c586959fbe3dbf21c0864c
Tamilarasan <[tamila015@gmail.com])>
2023-10-10 21:03:08 +0530

    Done project

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import "./App.css"
+import Pricetag from "./Components/pricetag";
+
+function App() {
+  const data = [
+    {
+      plan: "FREE",
+      price: "$0",
+      user: "Single User",
+      highlight: false,
+      userEnabler: true,
+      storage: "5GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: false,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: false,
+      subDomain: "Free Subdomain",
+      subDomainEnabler: false,
+      subDomainHighlight: false,
+      statusPlan: "Monthly Status Reports",
+      statusPlanEnabler: false,
+    },
+    {
+      plan: "PLUS",
+      price: "$9",
+      user: "5 User",
+      highlight: true,
+      userEnabler: true,
+      storage: "50GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: "Free Subdomain",
+      subDomainEnabler: true,
+      subDomainHighlight: false,
+      statusPlan: "Monthly Status Reports",
+      statusPlanEnabler: false,
+    },
+    {
+      plan: "PRO",
+      price: "$49",
+      user: "Unlimited User",
+      highlight: true,
+      userEnabler: true,
+      storage: "150GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: "Unlimited Free Subdomain",
+      subDomainEnabler: true,
+      subDomainHighlight: true,
+      statusPlan: "Monthly Status Reports",
+      statusPlanEnabler: true,
+    },
+  ];
+  return (
+    <>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+          {/* Approach1  */}
+            {/* <Pricetag value={data[0]} />
+            <Pricetag value={data[1]} />
+            <Pricetag value={data[2]} /> */}
+
+          {/* Approach2 */}
+
+            {data.map((e, i) => {
+              return <Pricetag value={e} key={i} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
